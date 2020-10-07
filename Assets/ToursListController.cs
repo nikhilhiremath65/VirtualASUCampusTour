99af6296b4ae6d09bfbe3abb1b06453ba259be38
Somesh S <50901702+someshs301195@users.noreply.github.com>
2020-10-06 17:54:28 -0700

    US77-Task137 Added a button to make tour and schedule list item prefab clickable

---
+++ Assets/ToursListController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TourListController : MonoBehaviour
+{
+
+    public GameObject ContentPanel;
+    public GameObject ListItemPrefab;
+
+    ArrayList schedules;
+    // Start is called before the first frame update
+    void Start()
+    {
+        schedules = new ArrayList();
+        for (int i = 0; i < 20; i++)
+        {
+            schedules.Add(new Schedule("Tour" + i));
+        }
+
+
+        foreach (Schedule s in schedules)
+        {
+            GameObject newSchedule = Instantiate(ListItemPrefab) as GameObject;
+
+            ListItemController controller = newSchedule.GetComponent<ListItemController>();
+            controller.Name.text = s.Name;
+
+            newSchedule.transform.parent = ContentPanel.transform;
+            newSchedule.transform.localScale = Vector3.one;
+
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
