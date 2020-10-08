13ed8c4a5bfe6785243c42cbff07682b2ec72027
Somesh S <50901702+someshs301195@users.noreply.github.com>
2020-10-08 10:22:21 -0700

    US77-Task137: Made schedule prefab clickable and made adjustments to diplay tour instead of schedules

---
+++ Assets/Scripts/ToursListController.cs
@@ -0,0 +1,35 @@
+﻿  using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToursListController : MonoBehaviour
+{
+
+    public GameObject ContentPanel;
+    public GameObject ListItemPrefab;
+
+    ArrayList tours;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tours = new ArrayList();
+        for (int i = 0; i < 20; i++)
+        {
+            tours.Add(new Schedule("Tour " + i));
+        }
+
+
+        foreach (Schedule t in tours)
+        {
+            GameObject newSchedule = Instantiate(ListItemPrefab) as GameObject;
+
+            ListItemController controller = newSchedule.GetComponent<ListItemController>();
+            controller.Name.text = t.Name;
+
+            newSchedule.transform.parent = ContentPanel.transform;
+            newSchedule.transform.localScale = Vector3.one;
+
+
+        }
+    }
+}
