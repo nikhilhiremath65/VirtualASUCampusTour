74f2cd14472ffe935759ed9f12388cb5de95273b
Narmada Ravali Namburi <narmadaravali.namburi@gmail.com>
2020-10-16 03:47:07 -0700

    US12-Task170 Fetched locations of a schedule and update list

---
+++ Assets/Scripts/DisplayScheduleLocations.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+public class DisplayScheduleLocations : MonoBehaviour
+{
+    public GameObject ContentPanel;
+    public GameObject ListItemPrefab;
+
+    DB_Details dbDetails;
+    DatabaseReference reference;
+    bool schedulesDisplayed;
+
+    ArrayList schedules;
+    // Start is called before the first frame update
+    void Start()
+    {
+        schedules = new ArrayList();
+
+        dbDetails = new DB_Details();
+
+        // Set up the Editor before calling into the realtime database.
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl(dbDetails.getDBUrl());
+
+        // Get the root reference location of the database.
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+        getScheduleData();
+        schedulesDisplayed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!schedulesDisplayed && schedules.Count > 0)
+        {
+            createScheduleList();
+        }
+    }
+
+    void getScheduleData()
+    {
+
+        reference.GetValueAsync().ContinueWith(task => {
+            if (task.IsFaulted)
+            {
+                // Handle the error...
+                Debug.Log("error fetching data");
+            }
+            else if (task.IsCompleted)
+            {
+                // getting schedules for a particular user.
+                DataSnapshot snapshot = task.Result.Child(dbDetails.getScheduleDBName()).Child("nhiremat").Child("FridaySchedule");
+
+                Dictionary<string, object> scheduleData = JsonConvert.DeserializeObject<Dictionary<string, object>>(snapshot.GetRawJsonValue());
+
+                foreach (string schedule in scheduleData.Keys)
+                {
+                    this.schedules.Add(new Schedule(schedule));
+                }
+            }
+        });
+    }
+
+    void createScheduleList()
+    {
+        foreach (Schedule s in schedules)
+        {
+            ListItemPrefab.SetActive(true);
+            GameObject newSchedule = Instantiate(ListItemPrefab) as GameObject;
+
+            ListItemController controller = newSchedule.GetComponent<ListItemController>();
+            controller.Name.text = s.Name;
+
+            newSchedule.transform.parent = ContentPanel.transform;
+            newSchedule.transform.localScale = Vector3.one;
+        }
+        schedulesDisplayed = true;
+    }
+}
