597ca73d00142847153d6fc697ead2f5d829b8f8
Ravikanth Reddy <ravikantreddy97@gmail.com>
2020-10-16 13:07:46 -0700

    US15-Task148: Code added and linked schedule item to the edit schedule scnene.

--- Assets/Scripts/ListItemController.cs
+++ Assets/Scripts/ListItemController.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Crud;
+using UnityEngine.SceneManagement;
 
 public class ListItemController : MonoBehaviour
 {
 
     public Text Name;
-
+    Singleton singleton;
     public void delete()
     {
         CrudOperations crud = new CrudOperations();
@@ -16,9 +17,12 @@ public class ListItemController : MonoBehaviour
         Destroy(gameObject);
     }
 
-    public void Edit()
+    public void Edit(string scenename)
     {
         // write edit logic here
+        singleton = Singleton.Instance();
+        singleton.setScheduleName(Name.text);
+        SceneManager.LoadScene(scenename);
     }
 
     public void nextScene()
