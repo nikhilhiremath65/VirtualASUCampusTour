0ed7fbfdd572fca106158c652e7e321c3d42736d
Narmada Ravali Namburi <narmadaravali.namburi@gmail.com>
2020-10-16 02:30:18 -0700

    US12-changes in schedule item prefab

--- Assets/Scripts/ListController.cs
+++ Assets/Scripts/ListController.cs
@@ -1,6 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 public class ListController : MonoBehaviour
 {
@@ -8,38 +13,94 @@ public class ListController : MonoBehaviour
     public GameObject ContentPanel;
     public GameObject ListItemPrefab;
 
+    DB_Details dbDetails;
+    DatabaseReference reference;
+    bool schedulesDisplayed;
+
     ArrayList schedules;
     // Start is called before the first frame update
     void Start()
     {
         schedules = new ArrayList();
-        // {
-        //     new Schedule("s1"),
-        //     new Schedule("s2")
-        // };
-        for (int i = 0; i < 20; i++)
-        {
-            schedules.Add(new Schedule("schedule" + i));
-        }
+
+        dbDetails = new DB_Details();
+
+        // Set up the Editor before calling into the realtime database.
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl(dbDetails.getDBUrl());
+
+        // Get the root reference location of the database.
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+        /*
+
+                for (int i = 0; i < 20; i++)
+                {
+                    schedules.Add(new Schedule("schedule" + i));
+                }
 
 
-        foreach (Schedule s in schedules)
-        {
-            GameObject newSchedule = Instantiate(ListItemPrefab) as GameObject;
+                foreach (Schedule s in schedules)
+                {
+                    GameObject newSchedule = Instantiate(ListItemPrefab) as GameObject;
 
-                ListItemController controller = newSchedule.GetComponent<ListItemController>();
-                controller.Name.text = s.Name;
+                        ListItemController controller = newSchedule.GetComponent<ListItemController>();
+                        controller.Name.text = s.Name;
 
-            newSchedule.transform.parent = ContentPanel.transform;
-            newSchedule.transform.localScale = Vector3.one;
+                    newSchedule.transform.parent = ContentPanel.transform;
+                    newSchedule.transform.localScale = Vector3.one;
 
 
-        }
+                }
+        */
+
+        getScheduleData();
+        schedulesDisplayed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!schedulesDisplayed && schedules.Count > 0)
+        {
+            createScheduleList();
+        }
+    }
 
+    void getScheduleData()
+    {
+
+        reference.GetValueAsync().ContinueWith(task => {
+            if (task.IsFaulted)
+            {
+                // Handle the error...
+                Debug.Log("error fetching data");
+            }
+            else if (task.IsCompleted)
+            {
+                DataSnapshot snapshot = task.Result.Child(dbDetails.getScheduleDBName()).Child("nhiremat");
+
+                Dictionary<string, object> scheduleData = JsonConvert.DeserializeObject<Dictionary<string, object>>(snapshot.GetRawJsonValue());
+
+                foreach (string schedule in scheduleData.Keys)
+                {
+                    this.schedules.Add(new Schedule(schedule));
+                }
+            }
+        });
+    }
+
+    void createScheduleList()
+    {
+        foreach (Schedule s in schedules)
+        {
+            ListItemPrefab.SetActive(true);
+            GameObject newSchedule = Instantiate(ListItemPrefab) as GameObject;
+
+            ListItemController controller = newSchedule.GetComponent<ListItemController>();
+            controller.Name.text = s.Name;
+
+            newSchedule.transform.parent = ContentPanel.transform;
+            newSchedule.transform.localScale = Vector3.one;
+        }
+        schedulesDisplayed = true;
     }
 }
