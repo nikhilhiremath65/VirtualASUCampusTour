2954369d4562b2077cee7ded26be66fb941bc2ab
Somesh S <50901702+someshs301195@users.noreply.github.com>
2020-10-16 01:36:02 -0700

    US78-Task164 : Completed linking scene and added some error handling features.

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,31 @@
+﻿public class Singleton
+{
+    private static Singleton _instance;
+
+    private string TourName;
+
+    protected Singleton()
+    {
+    }
+
+    public static Singleton Instance()
+    {
+
+        if (_instance == null)
+        {
+            _instance = new Singleton();
+        }
+
+        return _instance;
+    }
+
+    public void setTourName(string TourName)
+    {
+        this.TourName = TourName;
+    }
+
+    public string getTourName()
+    {
+        return this.TourName;
+    }
+}
