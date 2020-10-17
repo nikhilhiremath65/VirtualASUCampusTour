96063e0ee79360088688b3b3bcadbe9ac149c1bb
Somesh S <50901702+someshs301195@users.noreply.github.com>
2020-10-17 10:07:58 -0700

    Implemented uploading schedule data to dataase.

---
+++ Assets/Scripts/LocationWithTime.cs
@@ -0,0 +1,14 @@
+﻿public class LocationWithTime
+{
+
+    public string Name;
+    public string Time;
+
+    public LocationWithTime(string name, string time)
+    {
+
+        Name = name;
+        Time = time;
+    }
+}
+
---
+++ Assets/Scripts/TimeDropDown.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeDropDown : MonoBehaviour
+{
+    public Dropdown HoursDropdown;
+    public Dropdown MinutesDropdown;
+
+    int[] hoursValues;
+    int[] minutesValues;
+
+    void Start()
+    {
+        PopulateList();
+    }
+
+    void PopulateList()
+    {
+        List<string> hours = new List<string>() { "hour" };
+        for (int i = 0; i < 24; i++)
+        {
+            hours.Add(i.ToString("00"));
+        }
+
+        List<string> minutes = new List<string>() { "minute" };
+        for (int i = 0; i < 12; i++)
+        {
+            minutes.Add((i*5).ToString("00"));
+        }
+
+        HoursDropdown.AddOptions(hours);
+        MinutesDropdown.AddOptions(minutes);
+    }
+}
