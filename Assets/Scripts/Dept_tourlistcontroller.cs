c4a1d34d07cec27e6c1a40d0575eccb092fa16f5
Priyanka <pzar1@asu.edu>
2020-10-23 01:31:15 -0700

    US#45 - Resolved conflicts in the code

--- Assets/Scripts/Dept_tourlistcontroller.cs
+++ Assets/Scripts/Dept_tourlistcontroller.cs
@@ -1,6 +1,11 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 public class Dept_tourlistcontroller : MonoBehaviour
 {
@@ -8,6 +13,32 @@ public class Dept_tourlistcontroller : MonoBehaviour
     public Text Name;
     Singleton singleton;
     public GameObject deletePanel;
+    public GameObject ContentPanel;
+    public GameObject ListItemPrefab;
+
+
+    DB_Details dbDetails;
+    DatabaseReference reference;
+    bool schedulesDisplayed;
+
+    ArrayList schedules;
+
+    void Start()
+    {
+        schedules = new ArrayList();
+
+        dbDetails = new DB_Details();
+
+        // Set up the Editor before calling into the realtime database.
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl(dbDetails.getDBUrl());
+
+        // Get the root reference location of the database.
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+        getScheduleData();
+        schedulesDisplayed = false;
+    }
+
     public void ConfirmDelete()
     {
         deletePanel.SetActive(true);
@@ -26,8 +57,8 @@ public class Dept_tourlistcontroller : MonoBehaviour
     public void Edit(string scenename)
     {
         // write edit logic here
-        singleton = Singleton.Instance();
-        singleton.SetScheduleName(Name.text);
+        //singleton = Singleton.Instance();
+        //singleton.SetScheduleName(Name.text);
         SceneManager.LoadScene(scenename);
     }
 
