653a45a0524a09fa965a3fcc378878d0f4c7593a
Priyanka <pzar1@asu.edu>
2020-11-21 18:28:57 -0700

    US#11 - Imported files to the US

---
+++ Assets/Scripts/Switch_button.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Switch_button : MonoBehaviour
+{
+    public GameObject MapCamera;
+    public GameObject ARCamera;
+    public Text ViewText;
+
+    // Start is called before the first frame update
+    [System.Obsolete]
+    void Start()
+    {
+        
+        if (ARCamera.active)
+        {
+            MapCamera.SetActive(false);
+            ViewText.text = "Map View";
+        }
+        else if(MapCamera.active)
+        {
+            ARCamera.SetActive(false);
+            ViewText.text = "AR View";
+        }
+        else
+        {
+            Debug.Log("No View selected");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
